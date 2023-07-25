2c458ceb7085e1787b544a8f902b7d7758604f97
Disha <dishatomariitkgp@gmail.com>
2023-07-25 12:14:00 +0530

    added charts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,15 @@ import Landing from "./Landing";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
 import ProtectedRoute from "./ProtectedRoute";
+import StatsContainer from "../components/StatsContainer";
+import ChartsContainer from "../components/ChartsContainer";
 
-export { Landing, Error, Dashboard, Register, ProtectedRoute };
+export {
+  Landing,
+  Error,
+  Dashboard,
+  Register,
+  ProtectedRoute,
+  StatsContainer,
+  ChartsContainer,
+};
